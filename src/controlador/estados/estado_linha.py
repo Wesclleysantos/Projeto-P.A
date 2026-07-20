64e3c3d8055e57100d7ab3579dae3c711f95665d
HenzoMarcelo <henzom805@gmail.com>
2026-07-20 18:51:37 -0300

    Implementa padrão State

---
+++ src/controlador/estados/estado_linha.py
@@ -0,0 +1,46 @@
+from controlador.estados.estado import Estado
+from modelo.linha import Linha
+
+
+class EstadoLinha(Estado):
+
+    def mouse_press(self, controlador, event):
+
+        controlador.figura_atual = Linha(
+            event.x,
+            event.y,
+            controlador.cores.obter_cor_borda(),
+            controlador.cores.obter_cor_preenchimento()
+        )
+
+    def mouse_move(self, controlador, event):
+
+        if controlador.figura_atual is None:
+            return
+
+        controlador.figura_atual.atualizar(
+            event.x,
+            event.y
+        )
+
+        controlador.view.canvas.desenhar_preview(
+            controlador.desenho,
+            controlador.figura_atual
+        )
+
+    def mouse_release(self, controlador, event):
+
+        if controlador.figura_atual is None:
+            return
+
+        if not controlador.figura_atual.incompleta():
+
+            controlador.desenho.adicionar(
+                controlador.figura_atual
+            )
+
+        controlador.figura_atual = None
+
+        controlador.view.canvas.desenhar(
+            controlador.desenho
+        )
---
+++ src/controlador/estados/estado_rabisco.py
@@ -0,0 +1,46 @@
+from controlador.estados.estado import Estado
+from modelo.rabisco import Rabisco
+
+
+class EstadoRabisco(Estado):
+
+    def mouse_press(self, controlador, event):
+
+        controlador.figura_atual = Rabisco(
+            event.x,
+            event.y,
+            controlador.cores.obter_cor_borda(),
+            controlador.cores.obter_cor_preenchimento()
+        )
+
+    def mouse_move(self, controlador, event):
+
+        if controlador.figura_atual is None:
+            return
+
+        controlador.figura_atual.atualizar(
+            event.x,
+            event.y
+        )
+
+        controlador.view.canvas.desenhar_preview(
+            controlador.desenho,
+            controlador.figura_atual
+        )
+
+    def mouse_release(self, controlador, event):
+
+        if controlador.figura_atual is None:
+            return
+
+        if not controlador.figura_atual.incompleta():
+
+            controlador.desenho.adicionar(
+                controlador.figura_atual
+            )
+
+        controlador.figura_atual = None
+
+        controlador.view.canvas.desenhar(
+            controlador.desenho
+        )
